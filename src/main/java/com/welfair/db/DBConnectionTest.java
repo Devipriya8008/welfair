72b9b6b706fb40d5f90954653abce33f00e62cdf
devi8008 <devipriya8008@gmail.com>
2025-04-30 22:45:56 +0530

    Describe what you changed

--- src/main/java/com/welfair/db/DBConnectionTest.java
+++ src/main/java/com/welfair/db/DBConnectionTest.java
@@ -3,14 +3,3 @@ package com.welfair.db;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class DBConnectionTest {
-    public static void main(String[] args) {
-        try {
-            Connection connection = DBConnection.getConnection();
-            System.out.println("Connection successful: " + connection);
-        } catch (SQLException e) {
-            System.out.println("Connection failed.");
-            e.printStackTrace();
-        }
-    }
-}
